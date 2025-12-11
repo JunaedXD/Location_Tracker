1d96d492a01582e576dbe41ff8a13213c3634029
Shekh Junaed <sheikhjunaed705@gmail.com>
2025-12-11 18:14:57 +0530

    Add files via upload

--- store.php
+++ store.php
@@ -1,8 +1,15 @@
 <?php
 
-$myfile = fopen("location.txt", "w") 
-$txt = "lat: " . $_GET["lat"] . "\nlong: " .$_GET["long"];
+$lat = $_GET["lat"];
+$long = $_GET["long"];
+
+$myfile = fopen("location.txt", "w");
+
+$txt = "lat: $lat\nlong: $long";
+
 fwrite($myfile, $txt);
 fclose($myfile);
+
+echo "saved";
 
 ?>
